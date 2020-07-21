5566fae947648d8ba139f4d305cf663ab1d49d27
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-07-21 16:36:53 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,20 @@
 class Room < ApplicationRecord
+  has_many :room_users
+  has_many :users,through: :room_users
 end
+
+
+
+
+# tweet model 
+# belong_to :user
+# has_many :comments
+
+# user model 
+# has_many :tweets
+# has_many :comments
+
+
+# comment model 
+# belongs_to :tweet
+# belong_to :user
